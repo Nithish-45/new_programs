9f16ddbd2efd1dc55e4d1425a34048e9acdad33d
Nithish-45 <nithishnithe2001@.com>
2023-09-12 23:07:13 +0530

    bubble sort program

---
+++ bubble_sort.java
@@ -0,0 +1,23 @@
+public class bubblesort {
+	void sort(int[] a) {
+		for(int i=0;i<a.length;i++) {
+			for(int j=0;j<a.length-i-1;j++) {
+				if (a[j]>a[j+1]) {
+					int temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+			}
+		}
+		System.out.println("the sorted array will be ");
+		for(int i=0;i<a.length;i++) {
+			System.out.print(a[i]);
+		}
+	}
+	public static void main(String[] args) {
+		bubblesort b=new bubblesort();
+		int a[]= {5,6,8,2,3,1,4,4,3};
+		b.sort(a);
+
+	}
+}
