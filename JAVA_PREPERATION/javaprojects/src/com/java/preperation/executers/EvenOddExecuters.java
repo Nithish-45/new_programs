e1a12d5092c908946777a2a858ceaa375124e2bf
Nithish-45 <nithishnithe2001@.com>
2023-11-10 11:57:03 +0530

    all update

---
+++ JAVA_PREPERATION/javaprojects/src/com/java/preperation/executers/EvenOddExecuters.java
@@ -0,0 +1,10 @@
+package com.java.preperation.executers;
+
+import com.java.preperation.methods.EvenOdd;
+
+public class EvenOddExecuters {
+	public static void main(String[] args) {
+//		EvenOdd ref=new EvenOdd();
+		EvenOdd.EvenOrodd(0);
+}
+}
---
+++ JAVA_PREPERATION/javaprojects/src/com/java/preperation/methods/EvenOdd.java
@@ -0,0 +1,15 @@
+package com.java.preperation.methods;
+
+public class EvenOdd {
+	
+		public static void EvenOrodd(int a) {
+			if(a%2==0) {
+				System.out.println("even");
+			}
+			else {
+				System.out.println("odd");
+			}
+		
+		}
+
+}
