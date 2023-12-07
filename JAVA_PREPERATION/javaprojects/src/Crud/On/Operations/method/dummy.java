afcc4559541069245e28b6382c63fa2917976d56
Nithish-45 <nithishnithe2001@.com>
2023-12-07 15:32:42 +0530

    update all

---
+++ JAVA_PREPERATION/javaprojects/src/Crud/On/Operations/method/dummy.java
@@ -0,0 +1,38 @@
+package Crud.On.Operations.method;
+
+public class dummy {
+
+	public void replace(String a,String b,String c) {
+		int ar[]=new int[10];
+		int j=0,k=0;
+		String result="";
+		for(int i=0;i<a.length();i++) {
+			if(a.charAt(i)==b.charAt(j)) {
+				
+				j++;
+				if(j==b.length()) {
+					
+					
+					ar[k]=i-j+1;
+//					System.out.println(ar[k]);
+					 j=0;
+					 k++;
+					}
+				}
+		}
+		k=0;
+		for(int i=0;i<a.length();i++) {
+			
+			if(i==ar[k]) {
+				result+=c;
+				k++;
+				i=i+b.length()-1;
+			}else {
+				result+=a.charAt(i);
+			}
+		}
+		System.out.println(result);
+		
+	}	
+
+}
