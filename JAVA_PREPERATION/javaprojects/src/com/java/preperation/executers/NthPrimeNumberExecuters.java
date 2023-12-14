fb0e836a4541e27bb7035e04527e681d21465ea8
Nithish-45 <nithishnithe2001@.com>
2023-12-14 11:33:57 +0530

    update all

---
+++ JAVA_PREPERATION/javaprojects/src/com/java/preperation/executers/NthPrimeNumberExecuters.java
@@ -0,0 +1,12 @@
+package com.java.preperation.executers;
+
+import com.java.preperation.methods.NthPrimeNumber;
+
+public class NthPrimeNumberExecuters {
+	public static void main(String[] args) {
+		NthPrimeNumber ref =new NthPrimeNumber();
+		System.out.println(ref.nthprime(10));
+		
+	}
+
+}
