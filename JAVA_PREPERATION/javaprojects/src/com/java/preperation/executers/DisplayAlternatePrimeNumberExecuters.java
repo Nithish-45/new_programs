41a29c2c01adc732eac1181404b4cf357ae1ba28
Nithish-45 <nithishnithe2001@.com>
2023-12-15 09:05:56 +0530

    update all

---
+++ JAVA_PREPERATION/javaprojects/src/com/java/preperation/executers/DisplayAlternatePrimeNumberExecuters.java
@@ -0,0 +1,10 @@
+package com.java.preperation.executers;
+
+import com.java.preperation.methods.DisplayAlternatePrimeNumber;
+
+public class DisplayAlternatePrimeNumberExecuters {
+	public static void main(String[] args) {
+		DisplayAlternatePrimeNumber ref= new DisplayAlternatePrimeNumber();
+		ref.displayaltprime(100);
+	}
+}
