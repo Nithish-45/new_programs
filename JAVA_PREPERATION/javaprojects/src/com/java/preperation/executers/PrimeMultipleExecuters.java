c2da7d25dba9683c50d1f928f5b6596cc399c687
Nithish-45 <nithishnithe2001@.com>
2023-12-27 21:51:57 +0530

    update all

---
+++ JAVA_PREPERATION/javaprojects/src/com/java/preperation/executers/PrimeMultipleExecuters.java
@@ -0,0 +1,10 @@
+package com.java.preperation.executers;
+
+import com.java.preperation.methods.PrimeMultiple;
+
+public class PrimeMultipleExecuters {
+	public static void main(String[] args) {
+		PrimeMultiple ref=new PrimeMultiple();
+		ref.primemulti(15);
+	}
+}
--- JAVA_PREPERATION/javaprojects/src/com/java/preperations/vechicle/driver/EncapsulationExdrive.java
+++ JAVA_PREPERATION/javaprojects/src/com/java/preperations/vechicle/driver/EncapsulationExdrive.java
@@ -1,5 +1,17 @@
 package com.java.preperations.vechicle.driver;
 
+import com.java.preperations.vechicle.fuel.EncapsulationEx;
+
 public class EncapsulationExdrive {
+	
+		public static void main(String[] args) {
+			EncapsulationEx s=new EncapsulationEx();
+		
+//			System.out.println(s.city);
+			System.out.println(s.getname());
+			System.out.println(s.getage());
+			System.out.println(s.setage(5));
+			System.out.println(s.setname("nithi"));
+		}
 
 }
